2d43f25b21809ed5bea0bf25977448fc465dbdfb
“Claudio <aclaudiojj@gmail.com>
2019-08-12 21:08:47 -0300

    heroku error

--- src/Entities/Tournament.php
+++ src/Entities/Tournament.php
@@ -6,6 +6,9 @@ use \Illuminate\Database\Eloquent\Model;
 
 class Tournament extends Model
 {
+
+    protected $table = 'tournament';
+
     /**
      * The attributes that are mass assignable.
      *
